cc6d5984a0d32ee30889ded2493a7354340b57ca
vamsi <92839681+vamc0709@users.noreply.github.com>
2022-06-15 12:52:05 +0530

    commit3

--- pages/Log.vue
+++ pages/Log.vue
@@ -148,30 +148,30 @@ export default {
       //   goals: [],
 
       pickerOptions: {
-        shortcuts: [
-          {
-            text: 'Today',
-            onClick(picker) {
-              picker.$emit('pick', new Date())
-            },
-          },
-          {
-            text: 'Yesterday',
-            onClick(picker) {
-              const date = new Date()
-              date.setTime(date.getTime() - 3600 * 1000 * 24)
-              picker.$emit('pick', date)
-            },
-          },
-          {
-            text: 'A week ago',
-            onClick(picker) {
-              const date = new Date()
-              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
-              picker.$emit('pick', date)
-            },
-          },
-        ],
+        // shortcuts: [
+          // {
+          //   text: 'Today',
+          //   onClick(picker) {
+          //     picker.$emit('pick', new Date())
+          //   },
+          // },
+          // {
+          //   text: 'Yesterday',
+          //   onClick(picker) {
+          //     const date = new Date()
+          //     date.setTime(date.getTime() - 3600 * 1000 * 24)
+          //     picker.$emit('pick', date)
+          //   },
+          // },
+          // {
+          //   text: 'A week ago',
+          //   onClick(picker) {
+          //     const date = new Date()
+          //     date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+          //     picker.$emit('pick', date)
+          //   },
+          // },
+        // ],
       },
       // value1: '',
       // value2: '',
